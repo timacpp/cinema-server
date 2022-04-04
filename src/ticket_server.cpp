d08e2237e0dc4847f2a454c7b52ebc4010948b42
Tima <timathevedmed@gmail.com>
2022-04-04 11:00:25 +0200

    Implement server constructor

--- src/ticket_server.cpp
+++ src/ticket_server.cpp
@@ -1,4 +1,8 @@
 #include <fstream>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "flags.h"
 #include "ensure.h"
@@ -15,9 +19,9 @@ public:
     static constexpr uint16_t DFLT_PORT = 2022;
 
     TicketServer(const std::string& file, uint16_t port, uint32_t timeout) {
-        this->set_port(port);
+        this->bind_socket(port);
         this->set_timeout(timeout);
-        this->set_tickets(file);
+        this->initialize_database(file);
     }
 
     void run() {
@@ -25,29 +29,47 @@ public:
     }
 
 private:
-    uint16_t port;
+    int socket_fd = -1;
     uint32_t timeout;
     std::unordered_map<std::string, size_t> tickets;
 
-    void set_port(uint16_t port) {
-        ensure(MIN_PORT <= port && port <= MAX_PORT, "Invalid port value");
-        this->port = port;
-    }
-
-    void set_timeout(uint32_t timeout) {
-        ensure(MIN_TIMEOUT <= timeout && timeout <= MAX_TIMEOUT, "Invalid timeout value");
-        this->timeout = timeout;
+    void set_timeout(uint32_t _timeout) {
+        ensure(MIN_TIMEOUT <= _timeout
+               && _timeout <= MAX_TIMEOUT, "Invalid timeout value");
+        this->timeout = _timeout;
     };
 
-    void set_tickets(const std::string& file) {
-        std::ifstream input(file);
-        ensure(input.is_open(), "File", file, "does not exist");
+    void initialize_database(const std::string& file_db) {
+        std::ifstream file(file_db);
+        ensure(file.is_open(), "File", file_db, "does not exist");
 
-        for (std::string film, tickets_count; getline(input, film); ) {
-            if (getline(input, tickets_count)) {
+        for (std::string film, tickets_count; getline(file, film); ) {
+            if (getline(file, tickets_count)) {
                 std::stringstream(tickets_count) >> tickets[film];
             }
         }
+    }
+
+    void bind_socket(uint16_t port) {
+        ensure(MIN_PORT <= port && port <= MAX_PORT, "Invalid port value");
+        this->socket_fd = socket(AF_INET, SOCK_DGRAM, 0); /* IPv4 UDP socket */
+        ensure(this->socket_fd > 0, "Failed to create a socket with port", port);
+        ensure_errno(this->bind_address(create_address(port)), "Failed to bind address");
+    }
+
+    int bind_address(const sockaddr_in& address) const {
+        ensure(this->socket_fd > 0, "Cannot bind address to uninitialized socket");
+        return bind(this->socket_fd, (sockaddr*) &address, (socklen_t) sizeof(address));
+    }
+
+    static sockaddr_in create_address(uint16_t port) {
+        sockaddr_in address;
+
+        address.sin_family = AF_INET;
+        address.sin_port = htons(port);
+        address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        return address;
     }
 };
 
