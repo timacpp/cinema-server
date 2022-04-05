b6bb970af5eec06189ce66efb28ea9fbdc728a87
Tima <timathevedmed@gmail.com>
2022-04-05 11:08:58 +0200

    Separate tools for debug

--- src/ensure.h
+++ src/ensure.h
@@ -4,10 +4,27 @@
 #include <iostream>
 
 template<typename Arg, typename... Args>
+inline void display(const std::string& type, const Arg& head, const Args&... args) {
+    ((std::cerr << type + ": " << head) <<
+            ... << (std::cerr << " ", args)) << std::endl;
+}
+
+template<typename Arg, typename... Args>
+inline void alert(const Arg& head, const Args&... args) {
+    display("Error", head, args...);
+}
+
+template<typename Arg, typename... Args>
+inline void warn(const Arg& head, const Args&... args) {
+#ifndef NDEBUG
+    display("Warning", head, args...);
+#endif
+}
+
+template<typename Arg, typename... Args>
 inline void ensure(bool condition, const Arg& head, const Args&... args) {
     if (!condition) {
-        ((std::cerr << "Error: " << head) <<
-            ... << (std::cerr << " ", args)) << std::endl;
+        alert(head, args...);
         exit(EXIT_FAILURE);
     }
 }
