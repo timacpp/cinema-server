feca6ec68e95f549695a50b4ae4b85a0a26902a4
Tima <timathevedmed@gmail.com>
2022-04-09 21:30:02 +0200

    Simplify generic buffer writter

--- src/buffer.h
+++ src/buffer.h
@@ -5,38 +5,35 @@
 
 namespace {
     template<typename T>
-    using enable_if_string_t = std::enable_if_t<std::is_same_v<T, std::string>>;
-
-    template<typename T>
-    using enable_if_not_string_t = std::enable_if_t<!std::is_same_v<T, std::string>>;
-
-    template<typename T>
-    inline constexpr size_t bytes(const T& data, enable_if_not_string_t<T>* = nullptr) {
-        return sizeof(data);
-    }
-
-    template<typename T>
-    inline constexpr size_t bytes(const T& data, enable_if_string_t<T>* = nullptr) {
-        return data.size();
+    inline constexpr size_t bytes(const T& data) {
+        if constexpr (std::is_convertible_v<T, std::string>) {
+            return data.size();
+        } else {
+            return sizeof(data);
+        }
     }
 }
 
 template<typename Arg>
-inline size_t buffer_write(char* dest, Arg&& src, enable_if_not_string_t<Arg>* = nullptr) {
-    memcpy(dest, reinterpret_cast<char*>(&src), sizeof(src));
-    return sizeof(src);
+inline size_t buffer_write(char* dest, Arg&& src, size_t size) {
+    if constexpr (std::is_convertible_v<Arg, std::string>) {
+        memcpy(dest, const_cast<char*>(src.c_str()), size);
+    } else {
+        memcpy(dest, reinterpret_cast<char*>(&src), size);
+    }
+
+    return size;
 }
 
 template<typename Arg>
-inline size_t buffer_write(char* dest, Arg&& src, enable_if_string_t<Arg>* = nullptr) {
-    memcpy(dest, const_cast<char*>(src.c_str()), src.size());
-    return src.size();
+inline size_t buffer_write(char* dest, Arg&& src) {
+    return buffer_write(dest, src, bytes(src));
 }
 
 template<typename Arg, typename... Args>
 inline size_t buffer_write(char* dest, Arg&& src_head, Args&&... src_tail) {
-    return buffer_write(dest, std::forward<Arg>(src_head)) +
-           buffer_write(dest + bytes(src_head), std::forward<Args>(src_tail)...);
+    size_t written = buffer_write(dest, std::forward<Arg>(src_head));
+    return written + buffer_write(dest + written, std::forward<Args>(src_tail)...);
 }
 
 template<typename Arg>
