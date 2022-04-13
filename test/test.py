94b28bb6464b825a6f4dfa606932086a1261310b
Tima <timathevedmed@gmail.com>
2022-04-13 21:49:34 +0200

    Add more tests

--- test/test.py
+++ test/test.py
@@ -2,17 +2,21 @@ from test_commandline_params import test_commandline_params
 from test_small_correctness import test_small_correctness
 from test_big_correctness import test_big_correctness
 from test_limits import test_limits
+from test_empty_file import test_empty_file
 from test_reservation_timing_out import test_reservation_timing_out
+from test_invalid_requests import test_invalid_requests
+
 import os
 
 if __name__ == '__main__':
-    print("Każdy test powinien działać poniżej 20 sekund.")
     tests = [
         test_commandline_params,
-        # test_small_correctness,
-        # test_big_correctness,
-        # test_limits,
-        # test_reservation_timing_out,
+        test_small_correctness,
+        test_big_correctness,
+        test_limits,
+        test_reservation_timing_out,
+        test_empty_file,
+        test_invalid_requests
     ]
     
     try:
